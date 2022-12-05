210a5798153060cd16bd6cded2647a39d8d6bfae
David Godard <davidgodard76700@gmail.com>
2022-12-05 15:34:22 +0100

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,5 +5,18 @@ fetch('items.json')
         }
     })
     .then(data => {
+        data.items.forEach(element => {
 
-    })
+            const newDiv = document.createElement('div');
+            newDiv.innerHTML = `
+            <img src="${element.image_path}" alt="">
+            <p class="description">${element.item_name}</p>
+            <p class="price">60€</p>
+            <button>Ajouter au panier</button>`;
+            newDiv.classList.add('item','col-lg-2','col-5','mx-2','mx-lg-2')
+
+            document.querySelector('.maillot').appendChild(newDiv);
+
+        });
+    });
+
