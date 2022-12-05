b65f96f4c8b452d521159cdc96ab6eaa320dab73
David Godard <davidgodard76700@gmail.com>
2022-12-05 16:46:12 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,25 @@ fetch('items.json')
     })
     .then(data => {
         data.items.forEach(element => {
-
+     
             const newDiv = document.createElement('div');
             newDiv.innerHTML = `
-            <img src="${element.image_path}" alt="">
+            <img src="${element.image_path}" alt="${element.item_name}">
             <p class="description">${element.item_name}</p>
             <p class="price">60€</p>
             <button>Ajouter au panier</button>`;
             newDiv.classList.add('item','col-lg-2','col-5','mx-2','mx-lg-2')
+            newDiv.dataset.category = element.category
 
             document.querySelector('.maillot').appendChild(newDiv);
 
+
         });
     });
-
+function hideBall(){
+    let test = document.querySelectorAll('[data-category="ball"]')
+    test.forEach(element => {
+        element.style.display='none'
+    })
+    
+}
