d4cf8474dcc87ff8cca53c89616777028f7579ec
David Godard <davidgodard76700@gmail.com>
2022-12-06 11:00:37 +0100

    maj

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,24 +6,109 @@ fetch('items.json')
     })
     .then(data => {
         data.items.forEach(element => {
-     
+
             const newDiv = document.createElement('div');
             newDiv.innerHTML = `
             <img src="${element.image_path}" alt="${element.item_name}">
             <p class="description">${element.item_name}</p>
-            <p class="price">60€</p>
+            <p class="price">${element.price} €</p>
             <button>Ajouter au panier</button>`;
-            newDiv.classList.add('item','col-lg-2','col-5','mx-2','mx-lg-2')
+            newDiv.classList.add('item', 'col-lg-2', 'col-5', 'mx-2', 'mx-lg-2')
             newDiv.dataset.category = element.category
 
-            document.querySelector('.maillot').appendChild(newDiv);
+            document.querySelector('.produits').appendChild(newDiv);
 
 
         });
     });
-function hideBall(){
-    let test = document.querySelectorAll('[data-category="ball"]')
+function displayTshirt() {
+    let test = document.querySelectorAll('[data-category]')
     test.forEach(element => {
-        element.style.display='none'
-    })   
+        if (element.dataset.category == 'maillot') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+
+function displayShort() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'short') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displaySocks() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'chaussettes') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displaySocks() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'chaussettes') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displayCasquette() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'casquette') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displayEcharpe() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'echarpe') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displayBonnet() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'bonnet') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displayBall() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'ballon') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
+}
+function displayGoodies() {
+    let test = document.querySelectorAll('[data-category]')
+    test.forEach(element => {
+        if (element.dataset.category == 'goodies') {
+            element.style.display = 'flex';
+        } else {
+            element.style.display = 'none'
+        }
+    })
 }
